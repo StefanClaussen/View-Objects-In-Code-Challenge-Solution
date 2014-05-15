5ce3339c48190f27e8f12a63a273eb13d53c6f10
StefenClausen <stefanclaussen@me.com>
2014-05-15 11:38:29 +0100

    Complete ending lecture 215

--- View Objects In Code/STCViewController.m
+++ View Objects In Code/STCViewController.m
@@ -1,14 +1,12 @@
 //
 //  STCViewController.m
 //  View Objects In Code
-//
-//  Created by Stefan Claussen on 15/05/2014.
-//  Copyright (c) 2014 One foot after the other. All rights reserved.
-//
 
 #import "STCViewController.h"
 
 @interface STCViewController ()
+@property (strong, nonatomic) UILabel *myLabel;
+@property (strong, nonatomic) UITextField *myTextField;
 
 @end
 
@@ -18,6 +16,30 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    self.myLabel = [[UILabel alloc] init];
+    self.myLabel.frame = CGRectMake(20, 60, 280, 22);
+    [self.view addSubview:self.myLabel];
+    
+    self.myLabel.textColor = [UIColor blueColor];
+    self.myLabel.text = @"placeholder";
+    
+    self.myTextField = [[UITextField alloc]init];
+    self.myTextField.frame = CGRectMake(20, 110, 280, 30);
+    self.myTextField.placeholder = @"Type text here";
+    self.myTextField.backgroundColor = [UIColor lightGrayColor];
+    [self.view addSubview:self.myTextField];
+    
+    UIButton *myButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [myButton addTarget:self action:@selector(myButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
+    [myButton setTitle:@"press me!" forState:UIControlStateNormal];
+    myButton.frame = CGRectMake(20, 160, 280, 30);
+    [self.view addSubview:myButton];
+}
+
+- (void)myButtonPressed:(UIButton *)sender
+{
+    self.myLabel.text = self.myTextField.text;
+    [self.myTextField resignFirstResponder];
 }
 
 - (void)didReceiveMemoryWarning
